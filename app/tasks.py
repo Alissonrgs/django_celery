a794fe412df78d673ea21b7b2052bb162d172460
alissonrgs <asoares@morphus.com.br>
2018-04-27 16:24:33 -0300

    added fabric

--- app/tasks.py
+++ app/tasks.py
@@ -8,40 +8,40 @@ MIN = 60
 @shared_task(priority=0)
 def task_01():
     print("TASK 01 (priority=0) - Started")
-    time.delay(30)
+    time.sleep(30)
     print("TASK 01 (priority=0) - Finished")
 
 
 @shared_task(priority=5)
 def task_02():
     print("TASK 02 (priority=5) - Started")
-    time.delay(1 * MIN)
+    time.sleep(1 * MIN)
     print("TASK 02 (priority=5) - Finished")
 
 
 @shared_task(priority=10)
 def task_03():
     print("TASK 03 (priority=10) - Started")
-    time.delay(2 * MIN)
+    time.sleep(2 * MIN)
     print("TASK 03 (priority=10) - Finished")
 
 
 @shared_task(queue="beat", priority=0)
 def periodic_task_01():
     print("PERIODIC TASK 01 (priority=0) - Started")
-    time.delay(1 * MIN)
+    time.sleep(1 * MIN)
     print("PERIODIC TASK 01 (priority=0) - Finished")
 
 
 @shared_task(queue="beat", priority=10)
 def periodic_task_02():
     print("PERIODIC TASK 02 (priority=10) - Started")
-    time.delay(1 * MIN)
+    time.sleep(1 * MIN)
     print("PERIODIC TASK 02 (priority=10) - Finished")
 
 
 @shared_task(queue="transient")
 def transient_task_01():
     print("TRANSIENT TASK 01 (priority=10) - Started")
-    time.delay(5 * MIN)
+    time.sleep(5 * MIN)
     print("TRANSIENT TASK 01 (priority=10) - Finished")
---
+++ fabfile.py
@@ -0,0 +1,120 @@
+# coding:utf-8
+import codecs
+import os
+import tempfile
+
+from fabric.api import cd, env, execute, prefix, put, run, sudo, task
+
+GROUP = 'www-data'
+LOGS_DIR = '$HOME/logs'
+PID_DIR = '$HOME/pid'
+PROJECT_NAME = 'django_celery'
+PROJECT_DIR = '$HOME/%s' % PROJECT_NAME
+REPOSITORY = 'https://github.com/Alissonrgs/djang_celery.git'
+VENV_DIR = '$HOME/.venv'
+
+env.hosts = []
+
+
+def user():
+    "Get remote user"
+    return run('whoami').stdout.strip()
+
+
+def get_service_context():
+    USER = user()
+
+    return {
+        'USER': USER,
+        'GROUP': GROUP,
+        'LOGS_DIR': LOGS_DIR,
+        'PID_DIR': PID_DIR,
+        'PROJECT_NAME': PROJECT_NAME,
+        'PROJECT_DIR': PROJECT_DIR,
+        'VENV_DIR': VENV_DIR
+    }
+
+
+def update_service_file(service, context):
+    with codecs.open(os.path.join('etc/systemd', service), encoding='utf-8') as f:
+        template = f.read()
+
+    target = os.path.join('/lib/systemd/system/%s' % service)
+    content = template.format(**context)
+
+    tmp = tempfile.TemporaryFile()
+    tmp.write(content.encode('utf-8'))
+    tmp.seek(0)
+
+    put(tmp, target, use_sudo=True)
+    tmp.close()
+
+
+@task(name='update-service-systemd')
+def update_service_systemd(**kwargs):
+    """Create or update service files"""
+    context = get_systemd_context()
+    context.update(kwargs)
+
+    service_list = os.listdir('etc/systemd')
+    for service in service_list:
+        update_service_file(service, context)
+
+
+@task(name='service-status')
+def service_status(service):
+    result = sudo('systemctl show %s --no-pager' % service)
+    output = result.stdout.lower().strip()
+
+    return {
+        'active': 'activestate=active' in output,
+        'loaded': 'loadstate=loaded' in output,
+        'needs-daemon-reload': 'needdaemonreload=yes' in output,
+        'running': 'substate=running' in output
+    }
+
+
+def service_restart(service):
+    status = service_status(service)
+
+    # Not loaded? Loads!
+    if not status['loaded']:
+        sudo('systemctl enable ' + service)
+
+    # Not started? Starts!
+    if not status['running']:
+        sudo('systemctl start ' + service)
+    else:  # Started? Restarts!
+        sudo('systemctl restart ' + service)
+
+    return status
+
+
+@task(name='restart_systemd')
+def restart_systemd():
+    sudo('systemctl daemon-reload')
+    service_list = os.listdir('etc/systemd')
+    for service in service_list:
+        service_restart(service)
+
+
+@task
+def setup():
+    sudo('apt update && apt install git virtualenv')
+    run('virtualenv .venv')
+    run('git clone %s' % REPOSITORY)
+    with cd(PROJECT_DIR), prefix('source %s/bin/activate' % PROJECT_DIR):
+        run('git pull origin master')
+        run('pip install -r requirements.txt')
+        run('./manage.py migrate')
+    execute(update_service_systemd)
+
+
+@task
+def deploy():
+    with cd(PROJECT_DIR), prefix('source %s/bin/activate' % PROJECT_DIR):
+        run('git pull origin master')
+        run('pip install -r requirements.txt')
+        run("./manage.py migrate")
+    execute(update_service_systemd)
+    execute(restart_systemd)
